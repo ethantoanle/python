3021f71cff41a01024d3d0209379eab6a6008c83
Ethan Le <ethan.toanle@gmail.com>
2025-12-23 12:08:41 -0500

    Create a python file

---
+++ practice.py
@@ -0,0 +1,5 @@
+#Lab01
+print("Please enter the temperature in Celsius: ")
+cDegree = int(input())
+fDegree = (cDegree * 9 / 5) + 32
+print(cDegree, "degrees Celsius is ", fDegree, "degrees Fahrenheit")
